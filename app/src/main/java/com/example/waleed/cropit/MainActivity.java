11ad2efa636373aa163d3b0640a2085008c0fedc
Waleed Ahmed <wahmedwd3@gmail.com>
2018-11-24 13:46:28 -0500

    Load picture event handler (#7)
    * Added load picture event handler

--- app/src/main/java/com/example/waleed/cropit/MainActivity.java
+++ app/src/main/java/com/example/waleed/cropit/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.waleed.cropit;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView testImageView;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_PICK = 2;
 
     // UI Event Handlers
     private Button.OnClickListener takePictureListener = new Button.OnClickListener() {
@@ -33,7 +40,16 @@ public class MainActivity extends AppCompatActivity {
 
     private Button.OnClickListener loadPictureListener = new Button.OnClickListener() {
         public void onClick(View v) {
+            // Create intent for picking a photo from the gallery
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                // Bring up gallery to select a photo
+                startActivityForResult(intent, REQUEST_IMAGE_PICK);
+            }
         }
     };
 
@@ -44,6 +60,18 @@ public class MainActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             testImageView.setImageBitmap(imageBitmap);
+        }
+        else if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                testImageView.setImageBitmap(selectedImage);
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
